d69c223f1239d09f6cd74710c1f3da5f751e11d3
Yuri Kuichik <kuichikmis4enko@gmail.com>
2025-08-28 22:35:17 +0300

    added navigation

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 console.log('hello from index.js')
 
+const arrSections = document.querySelectorAll('section')
 
+function switchTab(e) {
+    const id = e.target?.id
+
+    if (!id) return;
+
+    hideVisiblePage()
+    showPage(id)
+}
+
+function showPage(id) {
+    const el = document.querySelector('.' + id)
+    el?.classList.replace('hidden', 'visible')
+}
+
+
+function hideVisiblePage() {
+    findVisiblePage()?.classList.replace('visible', 'hidden')
+}
+
+
+function findVisiblePage() {
+    return [...arrSections].find( (section) => {
+        return section.classList.contains('visible')
+    } )
+}
 
